a137809e6d4fb2c691c15938c866bcebf299d470
MQ_ <bartekmol2007@gmail.com>
2024-05-25 12:47:38 +0200

    pendrive fix

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <MyModal header="Random aah string moment" :text="text" theme="sale"/>
+  <!-- prop export -->
+  <p>{{title}}</p>
+  <input type="text" ref="name">
+  <!-- refs -->
+  <button @click="deviousbutton">make it RED</button>
+
+
+</template>
+<script>
+import MyModal from './components/MyModal.vue'
+export default {
+  name: 'App',
+  components: { MyModal },
+  data(){
+    return{
+      title: "we are doing text!?!?!",
+      text: "bonus text to learn binding with props"
+    }
+  },
+  methods:{
+    deviousbutton(){
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add('active');
+      this.$refs.name.focus()
+      // using refs to give an object a class
+
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+p{
+  font-size: 50px;
+  font-weight: 700;
+}
+
+.active{
+  background-color: red;
+}
+</style>
---
+++ src/components/MyModal.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="backdrop" v-if="isvisible">
+        <div class="modal" :class="{sale: theme === 'sale'}">
+            <div class="x-factor" @click="change"> x</div>
+            <p>Modal or smth idk in the middle of the tutorial</p>
+            
+        </div>
+    </div>
+    <p>{{ header }}</p>
+    <p>{{ text }}</p>
+</template>
+<script>
+    export default{
+        props: ["header", "text", "theme"],   //prop export
+        name: 'MyModal',
+        data(){
+            return {
+                isvisible: true
+            }
+        },
+        methods: {
+            change(){
+                this.isvisible = false
+                
+            }
+        }
+    }
+</script>
+<!-- or scoped -->
+<style>  
+    .backdrop{
+        background-color: rgba(0, 0, 0, 0.484);
+        width: 100%;
+        height: 1500px;
+        position: absolute;
+        margin-top: -60px;
+        display: flex;
+        justify-content: center;
+    }
+    .modal{
+        color: black;
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 20px;
+        background-color: white;
+        width: 450px;
+        height: 350px;
+        border: black solid 2px;
+        margin-top: 200px;
+        padding: 0 70px;
+    }
+    .x-factor{
+        margin-right: 20px;
+        float: right;
+        font-weight: 700;
+        
+    }
+    .modal p{
+        color: aliceblue;
+    }
+
+    .modal.sale{
+        background-color: rgb(9, 180, 115);
+        
+    }
+</style>
+
