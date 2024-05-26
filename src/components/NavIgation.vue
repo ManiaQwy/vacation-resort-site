40c83be028a7c0a3e8754376c735b9f01d7c3863
MQ_ <bartekmol2007@gmail.com>
2024-05-26 19:35:25 +0200

    nav

---
+++ src/components/NavIgation.vue
@@ -0,0 +1,9 @@
+<template>
+    <a class="menu active" href="#">O mnie</a><a class="menu" href="#">Aktualność</a><a href="#" class="menu">Hobby</a><a href="#" class="menu">Kontakt</a>
+</template>
+<script>
+
+</script>
+<style>
+
+</style>
